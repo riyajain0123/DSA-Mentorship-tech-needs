39656f10726bd1b0534377404092d7e2c0e48f20
riyajain0123 <144619329+riyajain0123@users.noreply.github.com>
2023-09-29 00:25:34 +0530

    Create Assign1_Q10.cpp

---
+++ Assign1_Q10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"enter number";
+    cin>>num;
+    for(i=1,i<=num,i++){
+        if (num%i==0){
+            cout<<i;
+        }
+    }
+    return 0;
+}
